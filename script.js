b163dcef5662ab7328ee261a430c42d30b8bd8dc
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-03-13 21:27:51 +0530

    updated

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ let dubaiText = document.getElementById('dubaitext');
 let burjkhalifa = document.getElementById('burjkhalifa');
 let stars = document.getElementById('stars');
 
-windq.addEventListener('scroll', () => {
+window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
     dubaiText.style.left = value * -2 + 'px';
     burjkhalifa.style.top = value * 1 + 'px';
+    stars.style.top = value * 1 + 'px';
 
 });
